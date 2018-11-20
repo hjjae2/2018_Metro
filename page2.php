d081238e6100cec829ca2284d7dac98126067cf8
kimzro <vkzlzmffjq@gmail.com>
2018-11-20 14:15:46 +0000

    ver2.0.1 (add modal func)

--- page2.php
+++ page2.php
@@ -1,11 +1,10 @@
 <?php
 #phpinfo();
-$host = "localhost";
+$host = "52.78.234.197";
 $user="test";
 $pass="1234";
 
 $connect = mysqli_connect($host, $user, $pass);
-
 
 mysqli_select_db($connect, "METRO_DB");
 $query="SELECT * from analysis_result_tb";
@@ -19,6 +18,7 @@ for($i=1;$i<=6;$i++){
     array_push($resultarray,$row[$i]);
 }
 $result_avg=round(array_sum($resultarray)/count($resultarray));
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -33,17 +33,76 @@ $result_avg=round(array_sum($resultarray)/count($resultarray));
 </head>
 <body>
     <div class="wrapper">
+        <a href="#open" class="detail">여기여기</a>
+        <button class="btn home" type="button" onclick="location.href='page1.html'"> </button>
+        <button class="btn refresh" type="button" onclick="location.href='page2.php'"> </button>
         <img class="img" src="img/METRO_2.png" alt="page1">
         <?php
-        switch($result_avg){
-            case 1: echo "<img class=\"grade\" src=\"img/grade1.png\">";
-                break;
-            case 2: echo "<img class=\"grade\" src=\"img/grade2.png\">";
-                break;
-            case 3: echo "<img class=\"grade\" src=\"img/grade3.png\">";
-                break;
-        }
+            switch($result_avg){
+                case 1: echo "<img class=\"grade\" src=\"img/grade1.png\">";
+                    break;
+                case 2: echo "<img class=\"grade\" src=\"img/grade2.png\">";
+                    break;
+                case 3: echo "<img class=\"grade\" src=\"img/grade3.png\">";
+                    break;
+            }
         ?>
+        <div class="white_content" id="open">
+            <div style="text-align:center">
+                <img class="train" src="img/tt.png">
+                <button type="button" onclick="location.href='#close'">닫기</button>
+                <?php
+                    switch($resultarray[0]){
+                        case 1: echo "<img class=\"box_1st\" src=\"img/grade1.png\">";
+                            break;
+                        case 2: echo "<img class=\"box_1st\" src=\"img/grade2.png\">";
+                            break;
+                        case 3: echo "<img class=\"box_1st\" src=\"img/grade3.png\">";
+                            break;
+                    }
+                    switch($resultarray[1]){
+                        case 1: echo "<img class=\"box_2nd\" src=\"img/grade1.png\">";
+                            break;
+                        case 2: echo "<img class=\"box_2nd\" src=\"img/grade2.png\">";
+                            break;
+                        case 3: echo "<img class=\"box_2nd\" src=\"img/grade3.png\">";
+                            break;
+                    }
+                    switch($resultarray[2]){
+                        case 1: echo "<img class=\"box_3rd\" src=\"img/grade1.png\">";
+                            break;
+                        case 2: echo "<img class=\"box_3rd\" src=\"img/grade2.png\">";
+                            break;
+                        case 3: echo "<img class=\"box_3rd\" src=\"img/grade3.png\">";
+                            break;
+                    }
+                    switch($resultarray[3]){
+                        case 1: echo "<img class=\"box_4th\" src=\"img/grade1.png\">";
+                            break;
+                        case 2: echo "<img class=\"box_4th\" src=\"img/grade2.png\">";
+                            break;
+                        case 3: echo "<img class=\"box_4th\" src=\"img/grade3.png\">";
+                            break;
+                    }
+                    switch($resultarray[4]){
+                        case 1: echo "<img class=\"box_5th\" src=\"img/grade1.png\">";
+                            break;
+                        case 2: echo "<img class=\"box_5th\" src=\"img/grade2.png\">";
+                            break;
+                        case 3: echo "<img class=\"box_5th\" src=\"img/grade3.png\">";
+                            break;
+                    }
+                    switch($resultarray[5]){
+                        case 1: echo "<img class=\"box_6th\" src=\"img/grade1.png\">";
+                            break;
+                        case 2: echo "<img class=\"box_6th\" src=\"img/grade2.png\">";
+                            break;
+                        case 3: echo "<img class=\"box_6th\" src=\"img/grade3.png\">";
+                            break;
+                    }
+                ?>
+            </div>
+        </div>
     </div>
 </body>
 </html>
